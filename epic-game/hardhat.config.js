c1496ede34dc9fc76a38aa6ad26a3b8669349528
ronkips <kipshillash@gmail.com>
2023-01-27 13:11:05 +0300

    updating the smart contract

--- epic-game/hardhat.config.js
+++ epic-game/hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const { PRIVATE_KEY, ALCHEMY_API_URL } = process.env;
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: ALCHEMY_API_URL,
+      accounts: [PRIVATE_KEY]
+    }
+  }
 };
--- epic-game/scripts/deploy.js
+++ epic-game/scripts/deploy.js
@@ -1,17 +1,40 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
   const gameContract = await gameContractFactory.deploy(
-    ["Leo", "Aang", "Pikachu"], // Names
+    ["Ken", "Sam", "Jay"], // Names
     [
       "https://i.imgur.com/pKd5Sdk.png", // Images
       "https://i.imgur.com/xVu4vFL.png",
       "https://i.imgur.com/WMB6g9u.png"
     ],
-    [100, 200, 300], // HP values
-    [100, 50, 25] // Attack damage values
+    [1000, 900, 800], // HP values
+    [80, 50, 25], // Attack damage values
+    "Hillary Kip", // Boss name
+    "https://i.imgur.com/AksR0tt.png", // Boss image
+    10000, // Boss hp
+    50 // Boss attack damage
   );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
+
+  let txn;
+  txn = await gameContract.mintCharacterNFT(0);
+  await txn.wait();
+  console.log("Minted NFT #1");
+
+  txn = await gameContract.mintCharacterNFT(1);
+  await txn.wait();
+  console.log("Minted NFT #2");
+
+  txn = await gameContract.mintCharacterNFT(2);
+  await txn.wait();
+  console.log("Minted NFT #3");
+
+  txn = await gameContract.mintCharacterNFT(1);
+  await txn.wait();
+  console.log("Minted NFT #4");
+
+  console.log("Done deploying and minting!");
 };
 
 const runMain = async () => {
